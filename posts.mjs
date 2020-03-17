40f333f83cb0b062f997ba75f4551cfa85835885
Simon Bering Larsen <XSISE@orsted.dk>
2020-03-17 11:11:39 +0100

    blog

---
+++ posts.mjs
@@ -0,0 +1,38 @@
+export const posts = [
+    {
+        title: 'Pepe!',
+        bodyText: 'her er pepe',
+        images: ['post-2020-03-14/tenor.gif'],
+        author: 'Lasse',
+        created: new Date("2020-03-14 12:00")
+    },
+    {
+        title: 'Hjemmeside - sådan gør man',
+        bodyText: 'her er vor dan man laver en hjemeside',
+        images: ["post-2020-03-15/simon 2.jpg"],
+        author: 'Lasse',
+        created: new Date("2020-03-15 12:00")
+    },
+    {
+        title: `Jonas...`,
+        bodyText: `jonases a'er`,
+        images: ['post-2020-03-16/jonas A.jpg'],
+        author: 'Lasse',
+        created: new Date("2020-03-16 09:00")
+    },
+    {
+        title: `Forskelligt...`,
+        bodyText: `noget vi har lavet`,
+        images: ['post-2020-03-16/pepe denmark.png',
+            'post-2020-03-16/kat.jpg',
+            'post-2020-03-16/skroe.jpg',
+            'post-2020-03-16/pid svin.jpg',
+            'post-2020-03-16/svampebob.jpg'],
+        author: 'Lasse',
+        created: new Date("2020-03-16 11:00")
+    }
+];
+
+
+
+
